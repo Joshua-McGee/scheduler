df3a681978c9be4cdf68c6ac3fca378ae1fac83c
Joshua McGee <jcooler.cooler@gmail.com>
2019-09-02 17:59:22 -0700

    made appointment folder to contain all the appointment componenets and added styles.scss, header.js, and index.js, made stories for all of these

---
+++ src/components/Appointment/Appointment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classnames from 'classnames';
+import "components/Appointment/styles.scss";
+
+
+export default function Appointment(props) {
+  // const appointmentClass = classnames("button", {
+  //   "button--confirm": props.confirm,
+  //   "button--danger": props.danger
+  // });
+  return (
+    <article className="appointment"></article>
+  );
+}
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classnames from 'classnames';
+//import Appointment from "components/Appointment";
+
+export default function Appointment(props) {
+  // const appointmentClass = classnames("button", {
+  //   "button--confirm": props.confirm,
+  //   "button--danger": props.danger
+  // });
+  return (
+    <article className="appointment">test</article>
+  );
+}
